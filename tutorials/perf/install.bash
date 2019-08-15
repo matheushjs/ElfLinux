026b27ccd705e746297bf3e069441fe658b9fa22
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-08-14 23:50:59 -0300

    Improve perf tutorial

---
+++ tutorials/perf/install.bash
@@ -0,0 +1,3 @@
+# install perf with this command
+# The packages with uname -r unravel to something like linux-tools-4.15.0-24-generic
+sudo apt install linux-tools-common linux-tools-generic linux-tools-`uname -r` linux-cloud-tools-`uname -r`
