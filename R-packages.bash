f66895cec74ef16f27b9a72e0a80b4eaa284f035
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-02-04 23:55:00 -0300

    Update required packages to install by apt

--- R-packages.bash
+++ R-packages.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install libopenblas-base
+sudo apt install -y libopenblas-base libopenblas-dev r-cran-curl
 sudo R -e 'install.packages(c("tseriesChaos", "EMD", "forecast", "rmutil", "truncnorm"))'
