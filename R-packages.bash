69f7cccc8080595c01ae5cccd9e470522901f0b6
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-02-10 17:01:59 -0300

    Add more packages and explanations

--- R-packages.bash
+++ R-packages.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
-sudo apt install -y libopenblas-base libopenblas-dev r-cran-curl
+
+function install_packages {
+	apt install -y libopenblas-base libopenblas-dev; # Some linear algebra packages require this
+	apt install -y r-cran-curl; # Idk which packages need this
+	apt install -y libxml2 libxml2-dev; # Required by roxygen
+};
+
+sudo bash -c "$(declare -f install_packages); install_packages";
 sudo R -e 'install.packages(c("tseriesChaos", "EMD", "forecast", "rmutil", "truncnorm", "rgl", "viridis", "devtools"))'
