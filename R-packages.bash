76a1b33b23bdf6c1cb81d59d2a850d48793802ba
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2020-02-19 00:50:57 -0300

    Add missing apt package

--- R-packages.bash
+++ R-packages.bash
@@ -4,7 +4,8 @@ function install_packages {
 	apt install -y libopenblas-base libopenblas-dev; # Some linear algebra packages require this
 	apt install -y r-cran-curl; # Idk which packages need this
 	apt install -y libxml2 libxml2-dev; # Required by roxygen
+	apt install -y libfreetype6-dev; # required by rgl
 };
 
 sudo bash -c "$(declare -f install_packages); install_packages";
-sudo R -e 'install.packages(c("tseriesChaos", "EMD", "forecast", "rmutil", "truncnorm", "rgl", "viridis", "devtools"))'
+sudo R -e 'install.packages(c("R.utils", "tseriesChaos", "EMD", "forecast", "rmutil", "truncnorm", "rgl", "viridis", "devtools"))'
